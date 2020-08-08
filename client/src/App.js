2fd4221e1413ec694e4ff5155b73065637002b79
masterkn48 <masterkn48@gmail.com>
2020-08-08 15:39:19 +0530

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { CircularProgress } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from "react-router-dom";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const Home = lazy(() => import("./component/Home"));
+const Login = lazy(() => import("./component/Auth/Login"));
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <Switch>
+        <Suspense fallback={<CircularProgress color="primary" />}>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Suspense>
+      </Switch>
+    </Router>
+  );
 };
 
-export default App;
+export default withRouter(App);
---
+++ client/src/component/Home/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import withRoot from "../../theme/withRoot";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default withRoot(Home);
